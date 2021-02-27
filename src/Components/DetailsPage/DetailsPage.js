b65d31a7de186c1ca7fe9f0ef940f86c33623142
Julianne Vela <cjvela@outlook.com>
2021-02-26 18:35:07 -0800

    fixed form layout on details page to match create page

--- src/Components/DetailsPage/DetailsPage.js
+++ src/Components/DetailsPage/DetailsPage.js
@@ -89,97 +89,97 @@ export default class DetailsPage extends Component {
 
 		return (
 			<main className='detailsPage'>
-                
+                <section>
+                    <form className='border-wrap' onSubmit={this.handleSubmit}>
+                        <label>
+                            Messier Number
+                            </label>
+                            <input
+                                value={messier_id}
+                                onChange={this.handleIdChange}
+                            />
+                        <label>
+                            New General Catalogue ID
+                            </label>
+                            <input
+                                value={ngc_ic_num}
+                                onChange={this.handleNGCChange}
+                            />
+                        <label>
+                            Image URL:
+                            </label>
+                            <input
+                                value={image}
+                                onChange={this.handleImageChange}
+                            />
+                        <label>
+                            Common Name: 
+                        </label>
+                            <input value={common_name} onChange={this.handleCNameChange}/>
+                        <label>
+                            Object Type:
+                            </label>
+                            <select
+                                value={type_id}
+                                onChange={this.handleTypeChange}>
+                                    <option value=''>Please Select a Type</option>
+                                {object_types.map(type => 
+                                    <option value={type.id} defaultValue={type_id === type.id}>     {type.type}
+                                    </option>
+                                    )
+                                }
+                            </select>
+                        <label>
+                            Distance from Earth (kly):
+                            </label>
+                            <input
+                                value={distance_from_earth_kly}
+                                onChange={this.handleDistanceChange}
+                            />
+                        <label>
+                            Constellation:
+                            </label>
+                            <input
+                                value={constellation}
+                                onChange={this.handleConsteChange}
+                            />
+                        <label>
+                            Apparent Magnitude:
+                            </label>
+                            <input
+                                value={apparent_mag}
+                                type='number'
+                                step={0.1}
+                                onChange={this.handleMagChange}
+                            />
+                        <label>
+                            RA:
+                            </label>
+                            <input
+                                value={right_asc}
+                                onChange={this.handleRAChange}
+                            />
+                        <label>
+                            Declination:
+                            </label>
+                            <input
+                                value={declination}
+                                onChange={this.handleDecChange}
+                            />
+                        <label>
+                            Observation Completed?
+                            </label>
+                            <input
+                                type='checkbox'
+                                value={observation_completed}
+                                onChange={this.handleObserveChange}
+                            />
+                        <button style={{alignSelf: 'center'}}>Change</button>
+                    </form>
+                        <button value={messier_id} onClick={this.handleDelete}>Delete</button>
 
-                <form className='border-wrap' onSubmit={this.handleSubmit}>
-					<label>
-						Messier Number
-						<input
-							value={messier_id}
-							onChange={this.handleIdChange}
-						/>
-					</label>
-					<label>
-						New General Catalogue ID
-						<input
-							value={ngc_ic_num}
-							onChange={this.handleNGCChange}
-						/>
-					</label>
-					<label>
-						Image URL:
-						<input
-							value={image}
-							onChange={this.handleImageChange}
-						/>
-					</label>
-					<label>
-                        Common Name: 
-                        <input value={common_name} onChange={this.handleCNameChange}/>
-					</label>
-					<label>
-						Object Type:
-						<select
-							value={type_id}
-							onChange={this.handleTypeChange}>
-                                <option value=''>Please Select a Type</option>
-                            {object_types.map(type => 
-                                <option value={type.id} defaultValue={type_id === type.id}>     {type.type}
-                                </option>
-                                )
-                            }
-						</select>
-					</label>
-					<label>
-						Distance from Earth (kly):
-						<input
-							value={distance_from_earth_kly}
-							onChange={this.handleDistanceChange}
-						/>
-					</label>
-					<label>
-						Constellation:
-						<input
-							value={constellation}
-							onChange={this.handleConsteChange}
-						/>
-					</label>
-					<label>
-						Apparent Magnitude:
-						<input
-							value={apparent_mag}
-                            type='number'
-                            step={0.1}
-							onChange={this.handleMagChange}
-						/>
-					</label>
-					<label>
-						RA:
-						<input
-							value={right_asc}
-							onChange={this.handleRAChange}
-						/>
-					</label>
-					<label>
-						Declination:
-						<input
-							value={declination}
-							onChange={this.handleDecChange}
-						/>
-					</label>
-					<label>
-						Observation Completed?
-						<input
-							type='checkbox'
-							value={observation_completed}
-							onChange={this.handleObserveChange}
-						/>
-					</label>
-					<button style={{alignSelf: 'center'}}>Change</button>
-				</form>
-                    <button value={messier_id} onClick={this.handleDelete}>Delete</button>
-
-				{/* <Form /> */}
+                    {/* <Form /> */}
+                </section>
 			</main>
 		);
 	}
